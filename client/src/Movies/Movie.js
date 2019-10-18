6cbbac822c2aa271901b721accaedeb5f863a376
fresco <fresco@frescos-iMac.local>
2019-10-18 00:47:43 -0400

    unyoinked and mvp ez clap

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -42,6 +42,7 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <div className="edit-button" onClick={() => this.props.history.push(`/update-movie/${this.state.movie.id}`)}>Edit</div>
       </div>
     );
   }
